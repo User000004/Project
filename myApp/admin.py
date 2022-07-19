f442a58cdd54822356464f4766d0324cc9e56daa
User000004 <108694950+User000004@users.noreply.github.com>
2022-07-19 13:50:46 +0300

    Update admin.py

--- myApp/admin.py
+++ myApp/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Photo, Video, Animal, Experiment, VideoRecording, Result 
+from .models import Animal, Experiment, VideoRecording, Result 
 
-admin.site.register(Photo)
-admin.site.register(Video)
 admin.site.register(Animal)
 admin.site.register(Experiment)
 admin.site.register(VideoRecording)
